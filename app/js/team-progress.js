104be02401168ce9a2c7b5acfaab9e0896b68ccf
mannycandidier <mannycandidier@gmail.com>
2016-04-27 12:52:26 +0800

    team-services

--- app/js/team-progress.js
+++ app/js/team-progress.js
@@ -1,47 +1,48 @@
-$('.a').animate({
-  width: "90%"
-}, 2000)
-
-$('.b').animate({
-  width: "95%"
-}, 2000)
-
-$('.c').animate({
-  width: "97%"
-}, 2000)
+  $('.a').animate({
+    width: "90%"
+  },2000)
 
 
-$('.d').animate({
-  width: "95%"
-}, 2000)
+  $('.b').animate({
+    width: "95%"
+  }, 2000)
+
+  $('.c').animate({
+    width: "97%"
+  }, 2000)
 
 
-$('.e').animate({
-  width: "85%"
-}, 2000)
+  $('.d').animate({
+    width: "95%"
+  }, 2000)
 
 
-$('.f').animate({
-  width: "90%"
-}, 2000)
-
-$('.g').animate({
-  width: "94%"
-}, 2000)
+  $('.e').animate({
+    width: "85%"
+  }, 2000)
 
 
-$('.h').animate({
-  width: "96%"
-}, 2000)
+  $('.f').animate({
+    width: "90%"
+  }, 2000)
 
-$('.i').animate({
-  width: "90%"
-}, 2000)
+  $('.g').animate({
+    width: "94%"
+  }, 2000)
 
-$('.j').animate({
-  width: "96%"
-}, 2000)
 
-$('.k').animate({
-  width: "96%"
-}, 2000)
+  $('.h').animate({
+    width: "96%"
+  }, 2000)
+
+  $('.i').animate({
+    width: "90%"
+  }, 2000)
+
+  $('.j').animate({
+    width: "96%"
+  }, 2000)
+
+  $('.k').animate({
+    width: "96%"
+  }, 2000)
