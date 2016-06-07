918c574e8ae7dd1b3f4c2788106c9c2e0ea01c16
Resty Joy Antiquin <resty.antiquin@gmail.com>
2016-06-07 21:57:27 +0800

    update on header, services, navigation

---
+++ static/js/material-index.js
@@ -0,0 +1,25 @@
+
+
+$(".card").each(function(){
+    var $mainButton = $(this).find(".main-button"),
+    $closeButton = $(this).find(".close-button"),
+    $buttonWrapper = $(this).find(".button-wrapper"),
+    $ripple = $(this).find(".ripple"),
+    $layer = $(this).find(".layered-content");
+
+    $mainButton.on("click", function(){
+        $ripple.addClass("rippling");
+        $buttonWrapper.addClass("clicked").delay(1500).queue(function(){
+            $layer.addClass("active");
+        });
+    });
+
+    $closeButton.on("click", function(){
+        $buttonWrapper.removeClass("clicked");
+        $ripple.removeClass("rippling");
+        $layer.removeClass("active");
+    });
+});
+
+// copy
+balapaCop("Material Overlay Animation", "#777");
