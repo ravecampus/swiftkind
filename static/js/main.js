9ddadf12a6eab69622fa499fc355cacd629926a0
Restie Antiquin <resty.antiquin@gmail.com>
2016-07-11 15:55:49 +0800

    create test for material ripple

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function( $ ) {
+jQuery(document).ready(function($) {
     $('.number').counterUp({
         delay: 1,
         time: 1000
@@ -6,39 +6,41 @@ jQuery(document).ready(function( $ ) {
 
     //nav-bar
 
-      $(".mobile-nav ul li.trigger-collapse a").click(function(event) {
-          $(".mobile-nav").collapse('hide');
-        });
+    $(".mobile-nav a").click(function(event) {
+      $(".hamburger").trigger("click");
+    
+       
+    });
 
     //tooltipster
     $('.tooltip').tooltipster({
         content: $('<span> This text is in bold case !</span>'),
-        position:'right'
+        position: 'right'
     });
 
 
     //
     new WOW().init();
-     $("#owl-theme").owlCarousel({
-        loop:true,
-        autoplay:5000,
-        autoplayHoverPause:true,
-        autoHeight:false,
-        singleItem:true,
-        responsive:{
-            0:{
-                items:1
+    $("#owl-theme").owlCarousel({
+        loop: true,
+        autoplay: 5000,
+        autoplayHoverPause: true,
+        autoHeight: false,
+        singleItem: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:1
+            600: {
+                items: 1
             },
-            1000:{
-                items:1
+            1000: {
+                items: 1
             }
         }
-      });
+    });
 
-         $(".hover").mouseleave(
+    $(".hover").mouseleave(
         function() {
             $(this).removeClass("hover");
         }
@@ -46,204 +48,187 @@ jQuery(document).ready(function( $ ) {
     );
 
 
-//set your google maps parameters
+    //set your google maps parameters
     var $latitude = 7.088303,
         $longitude = 125.616174,
         $map_zoom = 17;
 
     //google map custom marker icon - .png fallback for IE11
-    var is_internetExplorer11= navigator.userAgent.toLowerCase().indexOf('trident') > -1;
-    var $marker_url = ( is_internetExplorer11 ) ? 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-location.png' : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-location_1.svg';
-        
+    var is_internetExplorer11 = navigator.userAgent.toLowerCase().indexOf('trident') > -1;
+    var $marker_url = (is_internetExplorer11) ? 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-location.png' : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-location_1.svg';
+
     //define the basic color of your map, plus a value for saturation and brightness
     var $main_color = '#fff',
-        $saturation= -78,
-        $brightness= 67;
+        $saturation = -78,
+        $brightness = 67;
 
     //we define here the style of the map
-    var style= [ 
-        {
+    var style = [{
             //set saturation for the labels on the map
             elementType: "labels",
             stylers: [
-                {saturation: $saturation}
+                { saturation: $saturation }
             ]
-        },  
-        {   //poi stands for point of interest - don't show these lables on the map 
+        }, { //poi stands for point of interest - don't show these lables on the map 
             featureType: "poi",
             elementType: "labels",
             stylers: [
-                {visibility: "off"}
+                { visibility: "off" }
             ]
-        },
-        {
+        }, {
             //don't show highways lables on the map
             featureType: 'road.highway',
             elementType: 'labels',
             stylers: [
-                {visibility: "off"}
+                { visibility: "off" }
             ]
-        }, 
-        {   
+        }, {
             //don't show local road lables on the map
-            featureType: "road.local", 
-            elementType: "labels.icon", 
+            featureType: "road.local",
+            elementType: "labels.icon",
             stylers: [
-                {visibility: "off"} 
-            ] 
-        },
-        { 
+                { visibility: "off" }
+            ]
+        }, {
             //don't show arterial road lables on the map
-            featureType: "road.arterial", 
-            elementType: "labels.icon", 
+            featureType: "road.arterial",
+            elementType: "labels.icon",
             stylers: [
-                {visibility: "off"}
-            ] 
-        },
-        {
+                { visibility: "off" }
+            ]
+        }, {
             //don't show road lables on the map
             featureType: "road",
             elementType: "geometry.stroke",
             stylers: [
-                {visibility: "off"}
+                { visibility: "off" }
             ]
-        }, 
+        },
         //style different elements on the map
-        { 
-            featureType: "transit", 
-            elementType: "geometry.fill", 
-            stylers: [
-                { hue: $main_color },
-                { visibility: "on" }, 
-                { lightness: $brightness }, 
-                { saturation: $saturation }
-            ]
-        }, 
-        {
-            featureType: "poi",
-            elementType: "geometry.fill",
-            stylers: [
-                { hue: $main_color },
-                { visibility: "on" }, 
-                { lightness: $brightness }, 
-                { saturation: $saturation }
-            ]
-        },
-        {
-            featureType: "poi.government",
-            elementType: "geometry.fill",
-            stylers: [
-                { hue: $main_color },
-                { visibility: "on" }, 
-                { lightness: $brightness }, 
-                { saturation: $saturation }
-            ]
-        },
-        {
-            featureType: "poi.sport_complex",
-            elementType: "geometry.fill",
-            stylers: [
-                { hue: $main_color },
-                { visibility: "on" }, 
-                { lightness: $brightness }, 
-                { saturation: $saturation }
-            ]
-        },
-        {
-            featureType: "poi.attraction",
-            elementType: "geometry.fill",
-            stylers: [
-                { hue: $main_color },
-                { visibility: "on" }, 
-                { lightness: $brightness }, 
-                { saturation: $saturation }
-            ]
-        },
-        {
-            featureType: "poi.business",
-            elementType: "geometry.fill",
-            stylers: [
-                { hue: $main_color },
-                { visibility: "on" }, 
-                { lightness: $brightness }, 
-                { saturation: $saturation }
-            ]
-        },
         {
             featureType: "transit",
             elementType: "geometry.fill",
             stylers: [
                 { hue: $main_color },
-                { visibility: "on" }, 
-                { lightness: $brightness }, 
+                { visibility: "on" },
+                { lightness: $brightness },
                 { saturation: $saturation }
             ]
-        },
-        {
+        }, {
+            featureType: "poi",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: $main_color },
+                { visibility: "on" },
+                { lightness: $brightness },
+                { saturation: $saturation }
+            ]
+        }, {
+            featureType: "poi.government",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: $main_color },
+                { visibility: "on" },
+                { lightness: $brightness },
+                { saturation: $saturation }
+            ]
+        }, {
+            featureType: "poi.sport_complex",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: $main_color },
+                { visibility: "on" },
+                { lightness: $brightness },
+                { saturation: $saturation }
+            ]
+        }, {
+            featureType: "poi.attraction",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: $main_color },
+                { visibility: "on" },
+                { lightness: $brightness },
+                { saturation: $saturation }
+            ]
+        }, {
+            featureType: "poi.business",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: $main_color },
+                { visibility: "on" },
+                { lightness: $brightness },
+                { saturation: $saturation }
+            ]
+        }, {
+            featureType: "transit",
+            elementType: "geometry.fill",
+            stylers: [
+                { hue: $main_color },
+                { visibility: "on" },
+                { lightness: $brightness },
+                { saturation: $saturation }
+            ]
+        }, {
             featureType: "transit.station",
             elementType: "geometry.fill",
             stylers: [
                 { hue: $main_color },
-                { visibility: "on" }, 
-                { lightness: $brightness }, 
+                { visibility: "on" },
+                { lightness: $brightness },
                 { saturation: $saturation }
             ]
-        },
-        {
+        }, {
             featureType: "landscape",
             stylers: [
                 { hue: $main_color },
-                { visibility: "on" }, 
-                { lightness: $brightness }, 
+                { visibility: "on" },
+                { lightness: $brightness },
                 { saturation: $saturation }
             ]
-            
-        },
-        {
+
+        }, {
             featureType: "road",
             elementType: "geometry.fill",
             stylers: [
                 { hue: $main_color },
-                { visibility: "on" }, 
-                { lightness: $brightness }, 
+                { visibility: "on" },
+                { lightness: $brightness },
                 { saturation: $saturation }
             ]
-        },
-        {
+        }, {
             featureType: "road.highway",
             elementType: "geometry.fill",
             stylers: [
                 { hue: $main_color },
-                { visibility: "on" }, 
-                { lightness: $brightness }, 
+                { visibility: "on" },
+                { lightness: $brightness },
                 { saturation: $saturation }
             ]
-        }, 
-        {
+        }, {
             featureType: "water",
             elementType: "geometry",
             stylers: [
                 { hue: $main_color },
-                { visibility: "on" }, 
-                { lightness: $brightness }, 
+                { visibility: "on" },
+                { lightness: $brightness },
                 { saturation: $saturation }
             ]
         }
     ];
-        
+
     //set google map options
     var map_options = {
-        center: new google.maps.LatLng($latitude, $longitude),
-        zoom: $map_zoom,
-        panControl: false,
-        zoomControl: false,
-        mapTypeControl: false,
-        streetViewControl: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        scrollwheel: false,
-        styles: style,
-    }
-    //inizialize the map
+            center: new google.maps.LatLng($latitude, $longitude),
+            zoom: $map_zoom,
+            panControl: false,
+            zoomControl: false,
+            mapTypeControl: false,
+            streetViewControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            scrollwheel: false,
+            styles: style,
+        }
+        //inizialize the map
     var map = new google.maps.Map(document.getElementById('google-container'), map_options);
     //add a custom marker to the map                
     var marker = new google.maps.Marker({
@@ -256,17 +241,17 @@ jQuery(document).ready(function( $ ) {
     //add custom buttons for the zoom-in/zoom-out on the map
     function CustomZoomControl(controlDiv, map) {
         //grap the zoom elements from the DOM and insert them in the map 
-        var controlUIzoomIn= document.getElementById('cd-zoom-in'),
-            controlUIzoomOut= document.getElementById('cd-zoom-out');
+        var controlUIzoomIn = document.getElementById('cd-zoom-in'),
+            controlUIzoomOut = document.getElementById('cd-zoom-out');
         controlDiv.appendChild(controlUIzoomIn);
         controlDiv.appendChild(controlUIzoomOut);
 
         // Setup the click event listeners and zoom-in or out according to the clicked element
         google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
-            map.setZoom(map.getZoom()+1)
+            map.setZoom(map.getZoom() + 1)
         });
         google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
-            map.setZoom(map.getZoom()-1)
+            map.setZoom(map.getZoom() - 1)
         });
     }
 
@@ -277,16 +262,16 @@ jQuery(document).ready(function( $ ) {
     map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
 
     $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html, body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-          }
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
         }
-      });
+    });
 
 });
--- static/js/material-index.js
+++ static/js/material-index.js
@@ -1,15 +1,20 @@
-$(".card").each(function() {
+$(".card").each(function(){
     var $mainButton = $(this).find(".main-button"),
-        $buttonWrapper = $(this).find(".button-wrapper"),
-        $ripple = $(this).find(".ripple"),
-        $layer = $(this).find(".layered-content");
+    $closeButton = $(this).find(".close-button"),
+    $buttonWrapper = $(this).find(".button-wrapper"),
+    $ripple = $(this).find(".ripple"),
+    $layer = $(this).find(".layered-content");
 
-    $mainButton.on("click", function() {
+    $mainButton.on("click", function(){
         $ripple.addClass("rippling");
-        $buttonWrapper.addClass("clicked").delay(1500).queue(function() {
+        $buttonWrapper.addClass("clicked").delay(1500).queue(function(){
             $layer.addClass("active");
         });
     });
 
-
+    $closeButton.on("click", function(){
+        $buttonWrapper.removeClass("clicked");
+        $ripple.removeClass("rippling");
+        $layer.removeClass("active");
+    });
 });
